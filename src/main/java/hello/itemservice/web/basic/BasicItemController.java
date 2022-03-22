0ed6a64cd429b2bc4da2eb7e8686488316704dc7
Jeonminje <12181674.inhe.edu>
2022-03-22 22:21:35 +0900

    redirect example

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -39,10 +36,44 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-    @PostMapping ("/add")
-    public String save(){
-        return "xx";
+    //@PostMapping ("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model){
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item",item);
+
+        return "basic/item";
     }
+    @PostMapping ("/add")
+    public String addItemV2(@ModelAttribute("item") Item item){ // "item" 으로 모델을 넘겨줌. 그리고 "item" 조차 생략가능. 객체 앞글자만 소문자로 바꿔서 넘겨줌.
+        itemRepository.save(item);
+
+        //model.addAttribute("item",item); 생략가능 모델어트리뷰트 떄문에
+
+        return "basic/item";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
