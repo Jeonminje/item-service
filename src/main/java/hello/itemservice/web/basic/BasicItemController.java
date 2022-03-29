4d7e56ca05656aa78be7494caa662c5c60a13090
Inha <wjsalswp120@naver.com>
2022-03-29 19:29:10 +0900

    thymeleaf checkbox

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -3,6 +3,7 @@ package hello.itemservice.web.basic;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,14 +11,27 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor // 생성자 주입 알아서해줌
 public class BasicItemController {
 
     private final ItemRepository itemRepository;
+
+    @ModelAttribute("regions") // 어떤거든지 모델에 자동으로 addAttribute 해줌.
+    public Map<String, String > regions (){
+        Map<String, String> regions = new LinkedHashMap<>();
+
+        regions.put("SEOUL","서울");
+        regions.put("BUSAN","부산");
+        regions.put("JEJU","제주");
+        return regions;
+    }
 
     @GetMapping
     public String items(Model model){
@@ -29,11 +43,14 @@ public class BasicItemController {
     public String item(@PathVariable long itemId, Model model){
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item",item);
+
         return "basic/item";
     }
 
     @GetMapping("/add")
-    public String addForm(){
+    public String addForm(Model model){
+        model.addAttribute("item", new Item());
+
         return "basic/addForm";
     }
 
@@ -73,6 +90,10 @@ public class BasicItemController {
 
     @PostMapping ("/add")
     public String addItemV4(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes){
+
+        log.info("item.open={}",item.getOpen());
+        log.info("item.regions={}", item.getRegions());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId",savedItem.getId());
         redirectAttributes.addAttribute("status",true);
@@ -84,6 +105,7 @@ public class BasicItemController {
     public String editForm(@PathVariable Long itemId, Model model){
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item",item);
+
         return "basic/editForm";
     }
 
